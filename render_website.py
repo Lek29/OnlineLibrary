dde82a9efbda8bd8cdb9dbb8d348bacc194ea6b1
Lek <ligioner29@mail.ru>
2025-07-11 06:56:01 +0800

    integrate livereload

--- render_website.py
+++ render_website.py
@@ -1,18 +1,31 @@
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import json
+from livereload import Server
 
-with open('meta_data.json', 'r', encoding='utf-8') as f:
-    books = json.load(f)
+def on_reload():
+    print('Обнаружено изменение...')
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html'])
-)
-template = env.get_template('template.html')
+    with open('meta_data.json', 'r', encoding='utf-8') as f:
+        books = json.load(f)
 
-html = template.render(books=books)
+    env = Environment(
+        loader=FileSystemLoader('templates'),
+        autoescape=select_autoescape(['html']),
+        cache_size=0
+    )
+    template = env.get_template('template.html')
 
-with open('index.html', 'w', encoding='utf-8') as f:
-    f.write(html)
+    html = template.render(books=books)
 
-print("Готово! Откройте index.html")
+    with open('index.html', 'w', encoding='utf-8') as f:
+        f.write(html)
+
+    print("Готово! Обновлён index.html")
+
+if __name__ == '__main__':
+    on_reload()
+
+    server = Server()
+    server.watch('templates/template.html', on_reload)
+    server.watch('meta_data.json', on_reload)
+    server.serve(root='.', port=5500, liveport=35729)
