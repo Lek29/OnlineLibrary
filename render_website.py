8cf25a128f77560e0839f4d917bb612727916fd4
Lek <ligioner29@mail.ru>
2025-07-15 17:00:45 +0800

    Add paginator

--- render_website.py
+++ render_website.py
@@ -1,22 +1,23 @@
-from tkinter.tix import ListNoteBook
-
-from jinja2 import Environment, FileSystemLoader, select_autoescape
-import json
 import os
-from livereload import Server
+import json
+from jinja2 import Environment, FileSystemLoader, select_autoescape
 from more_itertools import chunked
+from livereload import Server
 
 
 def on_reload():
     print('Обнаружено изменение...')
 
-    os.makedirs('pages', exist_ok=True)
-
     with open('meta_data.json', 'r', encoding='utf-8') as f:
         books = json.load(f)
 
-    book_on_pages = 10
-    all_books_chunks = list(chunked(books, book_on_pages))
+    print(f"Загружено книг из meta_data.json: {len(books)}")
+
+    books_per_page = 10
+    all_books_chunks = list(chunked(books, books_per_page))
+    total_pages = len(all_books_chunks)
+
+    print(f"Всего чанков (страниц): {total_pages}")
 
     env = Environment(
         loader=FileSystemLoader('templates'),
@@ -25,23 +26,29 @@ def on_reload():
     )
     template = env.get_template('template.html')
 
+    os.makedirs('pages', exist_ok=True)
+
     for page_num, page_books in enumerate(all_books_chunks, 1):
         books_chunks_for_template = list(chunked(page_books, 2))
 
         if page_num == 1:
-            output_filename = 'index.html'
+            output_filepath = 'index.html'
         else:
-            output_filename = f'index{page_num}.html'
+            output_filepath = os.path.join('pages', f'page{page_num}.html')
 
-        output_filepath = os.path.join('pages', output_filename)
-
-
-        html = template.render(books_chunks=books_chunks_for_template)
+        html = template.render(
+            books_chunks=books_chunks_for_template,
+            total_pages=total_pages,
+            current_page=page_num
+        )
 
         with open(output_filepath, 'w', encoding='utf-8') as f:
             f.write(html)
 
-    print("Готово! Все страницы с книгами обновлены")
+        print(f"  Сгенерирован {output_filepath}")
+
+    print("Все страницы с книгами обновлены!")
+
 
 if __name__ == '__main__':
     on_reload()
